7ab26e9700702eb75384b6c5d6135fc64f943f46
DavidA. <dvd.agnld@gmail.com>
2024-01-23 20:36:38 +0800

    SplashScreen Draft, waiting for logo

--- src/screen/splash.js
+++ src/screen/splash.js
@@ -1,28 +1,64 @@
-import { View, Text , SafeAreaView } from 'react-native'
 import React, { useEffect } from 'react';
+import { View, Text, Image, StyleSheet, SafeAreaView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-
 const splash = () => {
+  const nav = useNavigation();
 
-    const nav = useNavigation()
-
-    useEffect(() => {
-        setTimeout(() => {
-           nav.replace('Login')
-        }, 5000);
-       }, []);
-
+  useEffect(() => {
+    setTimeout(() => {
+      nav.replace('Login');
+    }, 5000);
+  }, []);
 
   return (
-
-
-
-
-    <SafeAreaView>
-      <Text>Splash Screen</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.logoContainer}>
+        {/* <Image
+          source={require('LOGO NATIN DITO, WALA AKO FILE NG LOGO')} // Replace with the path to your app's logo
+          style={styles.logo}
+          resizeMode="contain"
+        /> */}
+        <Text style={styles.title}>
+          <Text style={styles.blackText}>M.</Text>
+          <Text style={styles.blueText}>Braille</Text>
+        </Text>
+        <Text style={styles.subtitle}>Empowering Access. Transforming Documents.</Text>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default splash
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+  },
+  logo: {
+    width: 100, // Adjust the width as needed
+    height: 100, // Adjust the height as needed
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  blackText: {
+    color: 'black',
+  },
+  blueText: {
+    color: '#062CD4',
+  },
+  subtitle: {
+    fontSize: 12,
+    textAlign: 'center',
+    marginTop: 5,
+    color: '#B6B6B6', // Adjust the color as needed
+  },
+});
+
+export default splash;
