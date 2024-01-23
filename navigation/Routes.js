96c2e60f5f524303cb9eee8d5e1f60d562d97149
Mark Pascua <markexequielpascua@gmail.com>
2024-01-23 23:41:10 +0800

    added logic

--- navigation/Routes.js
+++ navigation/Routes.js
@@ -5,10 +5,20 @@ import {NavigationContainer} from '@react-navigation/native';
 
 import AuthStack from './AuthStack';
 import AppStack from './AppStack';
+import { AuthContext } from './AuthProvider';
 
 const Routes = () => {
+  const {user, setUser} = useContext(AuthContext)
+  const [initializing, setInitializing] = useState(true);
+
+  const onAuthStateChanged = (user) => {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  };
+
   return (
     <NavigationContainer>
+      {/* {user ? <AppStack /> : <AuthStack />} Logic na kung may user sa APP na siya rekta */} 
       <AuthStack />
     </NavigationContainer>
   )
