6b10aeb22a1f2689caea43562c8e4e159c8e1b13
Mark Pascua <markexequielpascua@gmail.com>
2024-01-24 20:31:18 +0800

    App.js update

--- App.js
+++ App.js
@@ -7,23 +7,24 @@ import login from "./src/screen/login";
 import register from "./src/screen/register";
 import home from "./src/screen/home";
 import Routes from './navigation/Routes';
+import Providers from './navigation';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer independent= { true }>
-    <Stack.Navigator initialRouteName= "home" screenOptions={{ headerShown: false, }}>
-      
-      <Stack.Screen name="splash" component={ splash } />
-      <Stack.Screen name="login" component={ login } />
-      <Stack.Screen name="register" component={ register } />
-      <Stack.Screen name="home" component={ home } />
+      <Stack.Navigator initialRouteName= "home" screenOptions={{ headerShown: false, }}>
+        
+        <Stack.Screen name="splash" component={ splash } />
+        <Stack.Screen name="login" component={ login } />
+        <Stack.Screen name="register" component={ register } />
+        <Stack.Screen name="home" component={ home } />
 
-      
-    </Stack.Navigator>
-  </NavigationContainer>
-   // <Routes /> //Use this when trying out Routes
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  //  <Providers /> //Use this when trying out Routes
   );
 } ;
 
