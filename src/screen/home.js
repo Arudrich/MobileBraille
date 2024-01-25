8532fc6f74a5ff76534609997f3ba906662e2494
Mark Pascua <markexequielpascua@gmail.com>
2024-01-26 00:56:50 +0800

    Fixed name

--- src/screen/home.js
+++ src/screen/home.js
@@ -43,7 +43,7 @@ const home = () => {
 
         <Image resizeMode= 'contain' style = {styles.profile} source = {{ uri: 'https://scontent.fmnl2-2.fna.fbcdn.net/v/t39.30808-6/364163840_1714504549014921_1986141625276929911_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeHu0Y1HFrflmfV_gSJeTTR7iq6zHupNJJaKrrMe6k0klnEPVshyU8NVVXw6xuQinpL3kdSNRK-aKdcgqysr44Rp&_nc_ohc=p4rXIGBl0JoAX8kcyUy&_nc_ht=scontent.fmnl2-2.fna&oh=00_AfD0WFLZ_P89XCha7iF3OVoX3s5KQQX_fordPaDhKISO-Q&oe=65B4B99A' }} 
         />
-        <Text style = {styles.profileName}> {userData ? userData.fullname : "test(no user logged in)"} </Text>
+        <Text style = {styles.profileName}> {userData ? userData.fullname : "Loading..."} </Text>
         
       </View>
 
