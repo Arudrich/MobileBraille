b4f671208796554eb97f49c8ac2a05543bf69e47
Aru <aldrichdmacadangdang11@gmail.com>
2024-01-29 20:40:42 +0800

    PROFILE UI UPDATED

--- src/screen/profile.js
+++ src/screen/profile.js
@@ -12,12 +12,45 @@ import { MaterialIcons } from "@expo/vector-icons";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { AntDesign } from '@expo/vector-icons';
 
+// sections
+
+const SECTIONS = [
+  {
+    header: 'Preferences',
+    icon: 'settings',
+    items: [
+      { icon: 'globe', color: '#062CD4' , label: 'Language', type: 'link' },
+    ],
+  },
+
+  {
+    header: 'Help',
+    icon: 'help-circle',
+    items: [
+      { icon: 'flag', color: '#062CD4', label: 'Report Bug', type: 'link' },
+      { icon: 'mail', color: '#062CD4' , label: 'Contact Us', type: 'link' },
+    ],
+  },
+  {
+    header: 'Other Actions',
+    icon: 'align-center',
+    items: [
+      { icon: 'save', color: '#062CD4', label: 'Saved', type: 'link' },
+    ],
+  },
+];
+
+
 const profile = ({ navigation }) => {
   return (
 
 
     <SafeAreaView style={{ backgroundColor: "white", flex: 1 }}>
       <ScrollView style={styles.container}>
+
+        <Text style = {{fontSize: 20, fontWeight: "bold", alignSelf: "center"}}>EDIT PROFILE</Text>
+
+
         <View style={styles.profile}>
           <TouchableOpacity
             onPress={() => {
@@ -45,6 +78,183 @@ const profile = ({ navigation }) => {
             </View>
           </TouchableOpacity>
         </View>
+
+
+        <View style={styles.changeInfo}>
+
+        <Text
+            style={{
+              fontSize: 12,
+              fontWeight: "500",
+              color: "grey",
+              marginTop: 15,
+              color: "black",
+              paddingRight: 215,
+              fontWeight: "bold"
+            }}
+          >
+            Change Display Name
+          </Text>
+
+          <View
+            style={{
+              borderColor: "grey",
+              height: 45,
+              width: 350,
+              margin: 2,
+              borderRadius: 8,
+              paddingHorizontal: 12,
+              paddingVertical: 12,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+
+
+            <AntDesign name="user" size={19} color="blue" /> 
+
+
+
+          <TouchableOpacity>
+          <AntDesign name="edit" size={20} color="black" />
+          </TouchableOpacity>
+
+
+          </View>
+
+
+
+          <Text
+            style={{
+              fontSize: 12,
+              fontWeight: "500",
+              color: "grey",
+              marginTop: 18,
+              color: "black",
+              paddingRight: 240,
+              fontWeight: "bold"
+            }}
+          >
+            Change Password
+          </Text>
+
+          <View
+            style={{
+              borderColor: "grey",
+              height: 45,
+              width: 350,
+              margin: 12,
+              borderWidth: 0,
+              backgroundColor: 'white',
+              borderRadius: 8,
+              paddingHorizontal: 12,
+              paddingVertical: 12,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+
+
+            <FontAwesome5 name="key" size={18} color= "blue" />  
+
+          <TouchableOpacity>
+          <AntDesign name="edit" size={20} color="black" />
+          </TouchableOpacity>
+
+
+          </View>
+
+
+          <View style = {{ flex: 1}} >
+
+ 
+
+          {SECTIONS.map(({ header, items }) => (
+
+          <View style={styles.section} key={header}>
+            <Text style={styles.sectionHeader}>{header}</Text>
+            {items.map(({ label, icon, type, value, color }, index) => {
+              return (
+                <TouchableOpacity
+                  key={label}
+                  onPress={() => {
+                    // handle onPress
+                  }}>
+                  <View style={styles.row}>
+                    <View style={[styles.rowIcon, { backgroundColor: color }]}>
+                      <FeatherIcon color="white" name={icon} size={15} />
+                    </View>
+
+                    <Text style={styles.rowLabel}>{label}</Text>
+
+                    <View style={styles.rowSpacer} />
+
+                    {type === 'boolean' && <Switch value={value} />}
+
+                    {type === 'link' && (
+                      <FeatherIcon
+                        color="#0c0c0c"
+                        name="chevron-right"
+                        size={22}
+                      />
+                    )}
+
+                  </View>
+                  
+                </TouchableOpacity>
+
+              );
+            })}
+          </View>
+        ))}
+
+
+        </View>
+
+  
+        </View>
+
+
+        <View style = {{flex: 1, paddingBottom: 50, paddingHorizontal: 50,}}>
+
+
+          <TouchableOpacity
+    
+            style={{
+              flexDirection: "row" ,
+              backgroundColor: '#062CD4',
+              marginTop: 15,
+              height: 50,
+              borderRadius: 8 ,
+              alignItems: "center",
+              justifyContent: "center",
+              }} >
+
+            <Text
+              style={{
+                fontSize: 19,
+                color: 'white',
+                fontWeight: "500",
+              }} >
+
+              Log Out
+
+            </Text>
+
+          </TouchableOpacity>
+
+
+
+
+
+
+
+        </View>
+
+        
+
 
         
 
@@ -97,7 +307,59 @@ const styles = StyleSheet.create({
 
   },
 
+  // SECTION
 
+  section: {
+    paddingHorizontal: 24,
+
+  },
+
+  sectionHeader: {
+
+    paddingVertical: 12,
+    fontSize: 12,
+    fontWeight: '600',
+    color: '#000000',
+    textTransform: 'uppercase',
+    letterSpacing: 5, 
+  },
+
+  row: {
+
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 8,
+    marginBottom: 12,
+    paddingLeft: 12,
+    paddingRight: 12,
+    
+  },
+
+  rowIcon: {
+    width: 50,
+    height: 32,
+    borderRadius: 8,
+    marginRight: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  rowLabel: {
+    fontSize: 15,
+    fontWeight: '400',
+    color: 'black',
+    fontWeight: "bold",
+    paddingHorizontal: 190,
+    paddingLeft: .5
+  },
+  rowSpacer: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
 
 });
 
