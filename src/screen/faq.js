abea87816540cf51435a724467956e04fd6660b1
Aru <aldrichdmacadangdang11@gmail.com>
2024-02-05 15:09:43 +0800

    hehe

--- src/screen/faq.js
+++ src/screen/faq.js
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { AuthContext } from '../../navigation/AuthProvider';
+
 
 const faq = () => {
   return (
