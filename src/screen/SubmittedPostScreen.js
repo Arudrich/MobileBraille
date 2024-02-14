536a6bb7c0552e37eb1d69a4a68cfa9b46c9723e
MarkP <markexequielpascua@gmail.com>
2024-02-14 16:47:39 +0800

    submittedpost screen

--- src/screen/SubmittedPostScreen.js
+++ src/screen/SubmittedPostScreen.js
@@ -6,11 +6,13 @@ const SubmittedPostScreen = ({ route }) => {
   const { title, imageUrl, transcription, braille } = route.params;
 
   return (
-    <ScrollView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Title: {title}</Text>
-      <Image source = {{ uri: imageUrl }} style = {{ width: 200, height: 200, marginVertical: 20 }} resizeMode = 'contain' />
-      <Text>Transcription: {transcription}</Text>
-      <Text>Braille: {braille}</Text>
+    <ScrollView style={{ flex: 1, }}>
+      <View style={{ alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Title: {title}</Text>
+        <Image source = {{ uri: imageUrl }} style = {{ width: 200, height: 200, marginVertical: 20 }} resizeMode = 'contain' />
+        <Text>Transcription: {transcription}</Text>
+        <Text>Braille: {braille}</Text>
+      </View>
     </ScrollView>
   );
 };
