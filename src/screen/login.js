9b21270449f79077a3442a841921bc04845529ad
DavidA. <dvd.agnld@gmail.com>
2024-02-16 15:04:28 +0800

    login revamp, code cleaning, react native paper

--- src/screen/login.js
+++ src/screen/login.js
@@ -1,4 +1,4 @@
-import {View, Text, ScrollView, TouchableOpacity, TextInput, Image, Alert, KeyboardAvoidingView, Platform, StatusBar } from "react-native";
+import {View, Text, StyleSheet, ScrollView, TouchableOpacity, TextInput, Image, Alert, KeyboardAvoidingView, Platform, StatusBar } from "react-native";
 import React, { useContext, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
@@ -6,11 +6,11 @@ import { useNavigation } from "@react-navigation/native";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { authentication } from "../../FirebaseConfig";
 import { AuthContext } from "../../navigation/AuthProvider";
-
 // icons
 
-import { MaterialIcons } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons';
+// import { MaterialIcons, FontAwesome5 } from '@expo/vector-icons';
+//mats -david
+import { TextInput as PaperTextInput, Button } from 'react-native-paper';
 
 
 
@@ -18,15 +18,13 @@ import { FontAwesome5 } from '@expo/vector-icons';
 
 const Login = () => {
   const {login} = useContext(AuthContext);
-
   const nav = useNavigation();
-
   const [loginCredentials, setloginCredentials] = useState({
     email: "",
     password: "",
   });
   
-  const [isVisbile, setisVisbile] = useState(true);
+  const [isVisible, setisVisible] = useState(true);
 
   const { email, password } = loginCredentials;
 
@@ -41,171 +39,64 @@ const Login = () => {
   };
 
   return (
-
-
-      <SafeAreaView style = {{ backgroundColor: 'white', flex: 1 }}>
-        
-      <ScrollView>
-
-      <Text style={{ fontWeight: 'bold', fontSize: 48, color: '#062CD4', paddingTop: 71, paddingLeft: 130 }}> LOGIN
-      
-      </Text>
-
+      // <SafeAreaView style = {{ backgroundColor: 'white', flex: 1 }}>
+      <ScrollView style = {{backgroundColor: 'white', flex: 1}}>
+      <Text style={{ textAlign: 'center',fontWeight: 'bold', fontSize: 48, color: '#062CD4', paddingTop: 100 }}> LOGIN </Text>
         <View style={{ paddingHorizontal: 20, marginTop: 15, }}>
-         
-          <Text
-            style={{
-              fontSize: 16,
-              fontWeight: "400",
-              color: "black",
-              marginTop: 30,
-              paddingLeft: 12
-            }}
-          >
-            {" "}
-            Please enter your login information below
-          </Text>
+          <Text style={{justifyContent: 'center', fontSize: 16, fontWeight: '400', color: 'black', marginTop: 5, marginLeft: 15}}>
+           Please enter your login information below to access your account
+           </Text>
+           <Text style={styles.emailLabel}>Email</Text>
+<PaperTextInput
+  label="Please enter your email"
+  value={email}
+  onChangeText={(value) => setloginCredentials({ ...loginCredentials, email: value })}
+  mode="outlined"
+  style={styles.textInput}
+/>
 
-          <Text
-            style={{
-              fontSize: 16,
-              fontWeight: "400",
-              color: "black",
-              paddingLeft: 12
-              
-            }}
-          >
-            {" "}
-           to access your account
-          </Text>
-
-          <Text
-            style={{
-              fontSize: 16,
-              fontWeight: "500",
-              color: "black",
-              marginTop: 40,
-              paddingLeft: 12
-            }}
-          >
-            Email
-          </Text>
-          
-          <View
-            style={{
-              borderColor: "grey",
-              height: 59,
-              width: 340,
-              margin: 12,
-              borderWidth: 1,
-              borderRadius: 8,
-              paddingHorizontal: 12,
-              paddingVertical: 12,
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-
-          <MaterialIcons name="email" size={24} color= "blue" />
-
-          <TextInput
-            mode = "flat"
-            value={email}
-            onChangeText={(value) =>
-              setloginCredentials({ ...loginCredentials, email: value })
-            }
-            keyboardType="email-address"
-            style={{
-                fontSize: 16,
-                marginTop: 1,
-                flex: 0.9,
+<Text style={[styles.emailLabel, { marginTop: 10 }]}>Password</Text>
+<PaperTextInput
+  label="Enter your password"
+  value={password}
+  onChangeText={(value) => setloginCredentials({ ...loginCredentials, password: value })}
+  secureTextEntry={isVisible}
+  mode="outlined"
+  style={styles.textInput}
   
-            }}
-          />
-
-          </View>
-
-
-          
-
-          <Text
-            style={{
-              fontSize: 16,
-              fontWeight: "500",
-              color: "black",
-              marginTop: 10,
-              paddingLeft: 12
-            }}
-          >
-            Password
-          </Text>
-
-         
-          
-          <View
-            style={{
-              borderColor: "grey",
-              height: 60,
-              width: 340,
-              margin: 12,
-              borderWidth: 1,
-              borderRadius: 8,
-              paddingHorizontal: 12,
-              paddingVertical: 12,
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-
-            <FontAwesome5 name="key" size={24} color= "blue" />  
-
-            <TextInput
-              value={password}
-              onChangeText={(value) => {
-                setloginCredentials({ ...loginCredentials, password: value });
-              }}
-              secureTextEntry={isVisbile}
-              maxLength={20}
-              keyboardType="ascii-capable"
-              style={{
-                fontSize: 16,
-                marginTop: 1,
-                flex: 0.8,
-              }}
-            />
-            <Ionicons
-              onPress={() => {
-                setisVisbile(!isVisbile);
-              }}
-              name={
-                isVisbile == true
-                  ? "eye-off-outline"
-                  : "eye-outline" /* JS if else chuchu */
-              }
-              size={24}
-              color="black"
-            />
-          </View>
-
-          <Text
-            style={{
-              fontSize: 14,
-              fontWeight: "400",
-              color: "black",
-              marginTop: 15,
-              width: "95%",
-              paddingLeft: 12,
-              color: '#062CD4',
-              fontWeight: 'bold',
-
-            }} >
-
+  // right={
+  //   <PaperTextInput.Icon
+  //     name={isVisible ? 'eye-off-outline' : 'eye-outline'}
+  //     onPress={() => setisVisible(!isVisible)}
+  //     color="black"
+  //   />
+  // }
+/>
+          <TouchableOpacity
+           onPress={() => {
+            // Handle forgot password
+             }}
+           >
+            <Text style={{ fontSize: 16, fontWeight: 'bold', color: 'black', marginTop: 15, width: '95%', paddingLeft: 12, color: '#062CD4', fontWeight: 'bold' }}>
             Forgot Password?
           </Text>
+           </TouchableOpacity>
 
-          <TouchableOpacity
+           <Button
+          mode="contained"
+          onPress={() => login(email, password)}
+          style={{ marginTop: 15, height: 50, borderRadius: 100, backgroundColor: '#062CD4', justifyContent: 'center', alignItems: 'center' }}
+        >
+          Log In
+        </Button>
+
+        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 20, gap: 5 }}>
+          <Text style={{ fontSize: 16, color: 'black' }}>Don't have an account yet?</Text>
+          <TouchableOpacity onPress={() => nav.navigate('register')}>
+          <Text style={{ fontSize: 15, color: '#062CD4', fontWeight: 'bold' }}>Register here</Text>
+          </TouchableOpacity>
+          </View>
+          {/* <TouchableOpacity
             // onPress={loginUser}
             onPress={() => login(email, password)} //Use this Pag Providers na gamit sa App.js
             style={{
@@ -216,9 +107,9 @@ const Login = () => {
               borderRadius: 8 ,
               alignItems: "center",
               justifyContent: "center",
-              }} >
+              }} > */}
 
-            <Text
+            {/* <Text
               style={{
                 fontSize: 19,
                 color: 'white',
@@ -227,8 +118,8 @@ const Login = () => {
               Log In
             </Text>
 
-          </TouchableOpacity>
-          <View
+          </TouchableOpacity> */}
+          {/* <View
             style={{
               flexDirection: "row" ,
               justifyContent: "center",
@@ -237,14 +128,14 @@ const Login = () => {
               gap: 5,
               paddingTop: 24,
             }}
-          >
-            <Text style={{ fontSize: 16, color: "black" }} >Don't have an Account?</Text>
+          > */}
+            {/* <Text style={{ fontSize: 16, color: "black" }} >Don't have an Account?</Text>
             <TouchableOpacity
               onPress={() => {
                 nav.navigate("register");
               }}
-            >
-              <Text
+            > */}
+              {/* <Text
                 style={{
                   fontSize: 15,
                   color: '#062CD4',
@@ -253,14 +144,33 @@ const Login = () => {
               >
                 Sign Up
               </Text>
-            </TouchableOpacity>
-          </View>
+            </TouchableOpacity> */}
+      
         </View>
+        {/* </View> */}
       </ScrollView>
-      </SafeAreaView>
+      // </SafeAreaView>
 
   );
 };
+
+//label design -david
+const styles = StyleSheet.create({
+  emailLabel: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: 'black',
+    marginTop: 40,
+    paddingLeft: 15,
+  },
+
+  textInput: {
+    marginTop: 5,
+    backgroundColor: 'white',
+    width: 340,
+    margin: 10,
+  },
+});
 
 export default Login;
 
