70330b5c6764d5b7c1648b05a84f094527af58cb
Aru <aldrichdmacadangdang11@gmail.com>
2024-02-17 20:26:39 +0800

    UI UPDATE ABOUT AND HISTORY SCREEN.

--- src/screen/about.js
+++ src/screen/about.js
@@ -1,322 +1,299 @@
-import { View, Text, StyleSheet, Image,} from 'react-native'
+import { View, Text, ScrollView, StyleSheet, Image, Button } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { ScrollView } from 'react-native'
-import { TouchableOpacity } from 'react-native'
-import { counterEvent } from 'react-native/Libraries/Performance/Systrace'
+import { TouchableOpacity } from 'react-native';
 
-const About = () => {
+// lottie
+import LottieView from 'lottie-react-native';
+
+// DIMENSION COMPATIBILITY
+import { ScaledSheet } from 'react-native-size-matters';
+
+
+// TEXT REACTNATIVE PAPER
+import { TextInput } from 'react-native-paper';
+
+
+
+
+
+const about = () => {
+
+
+  const [text, setText] = React.useState("");
+
+
+
   return (
 
-    <ScrollView showsVerticalScrollIndicator={false} >
 
-      <View style = {styles.content}>
+    <ScrollView style = {styles.main} showsVerticalScrollIndicator={false} >
 
+      <View style = {styles.lottie}>
 
-      <Image
-      
-          style={styles.background}
-          source={require ('../assets/background/aboutbg.png')} 
-        />
+      <LottieView style = {{ paddingTop: 35, height: '30@s', alignSelf: 'center', alignItems: 'center', alignContent: 'center' }} source={require('../assets/lottie/about.json')} autoPlay loop />
 
       </View>
 
-      <View style = {styles.whoarewe}>
 
-        <Text style = {{ textAlign: 'justify', fontWeight: 'bold', fontSize: 20 }}><Text style = {{color: '#062CD4'}}>WHO</Text> ARE WE?</Text>
+      <View style = {styles.headerDeveloper}>
 
-        <Text style = {{textAlign: 'justify', fontSize: 12, paddingTop: 6, }}>Good day! we are a Computer Engineering Student from Technological Institute of the Philippines.</Text>
+        <Text style = {styles.headerTitleDeveloper}>DEVELOPERS</Text>
 
-        <Text style = {{ paddingTop: 10, textAlign: 'justify', fontWeight: 'bold', fontSize: 20 }}><Text style = {{color: '#062CD4'}}>WHAT</Text> IS MOBILE BRAILLE?</Text>
+         <ScrollView showsHorizontalScrollIndicator={false} horizontal={true} >
 
-        <Text style = {{textAlign: 'justify', fontSize: 12, paddingTop: 6, }}>Mobile Braille is an mobile application where we empower users to convert multiple modes of input
-          into Braille seamlessly. Our mission is to enhance accessibility for individuals
-          with visual impairments by providing a user-friendly and efficient transcription tool.</Text>
 
-        <Text style = {{ paddingTop: 10, textAlign: 'justify', fontWeight: 'bold', fontSize: 20 }}><Text style = {{color: '#062CD4'}}>VISION</Text> AND <Text style = {{color: '#062CD4'}}>MISSION</Text>  </Text>
+                <View style = {[styles.card, styles.cardElevated]}>
+                  <Image style= {styles.cardfavorites} source={require('../assets/DEVELOPERS/AGUINALDO.jpg')} />
+                  <Text style = {styles.name}>David Aguinaldo</Text>
+                  <Text style = {styles.electives}>Railway Engineering</Text>
+                </View>
 
-        <Text style = {{textAlign: 'justify', fontSize: 12, paddingTop: 6, }}>We envision a better life for Filipinos by empowering our students with the best globally competitive technological education in engineering, computing, and allied disciplines.</Text>
+                <View style = {[styles.card, styles.cardElevated]}>
+                  <Image style= {styles.cardfavorites} source={require('../assets/DEVELOPERS/PASCUA.jpg')} />
+                  <Text style = {styles.name}>Mark Pascua</Text>
+                  <Text style = {styles.electives}>System Administration</Text>
+                </View>
 
-        <Text style = {{textAlign: 'justify', fontSize: 12, paddingTop: 6, }}>Through digitalization and innovation in academic design and delivery, T.I.P. students, faculty, staff, and industry partners work together. </Text>
+                <View style = {[styles.card, styles.cardElevated]}>
+                  <Image style= {styles.cardfavorites} source={require('../assets/DEVELOPERS/GOMEZ.jpg')} />
+                  <Text style = {styles.name}>Rafael Gomez</Text>
+                  <Text style = {styles.electives}>Data Science</Text>
+                </View>
+
+                <View style = {[styles.card, styles.cardElevated]}>
+                  <Image style={styles.cardfavorites} source={require('../assets/DEVELOPERS/MACADANGDANG.jpg')} />
+                  <Text style = {styles.name}>Aldrich Macadangdang</Text>
+                  <Text style = {styles.electives}>Railway Engineering</Text>
+                </View>
+
+            </ScrollView>
+
+
+          </View>
+
+            
+
+
+           <View>
+
+              <Text style = {styles.mobileBraille}>Mobile Braille is an mobile application where we empower users to convert multiple modes of input
+              into Braille seamlessly. Our mission is to enhance accessibility for individuals
+              with visual impairments by providing a user-friendly and efficient transcription tool.</Text>
+
+
+
+           </View>
+
+
+           <View style = {styles.quickContact}>
+
+
+           <Text style = {styles.quickContactHeader}>QUICK CONTACT</Text>
+
+      
+
+           </View>
+
+
+           <View style = {styles.form}>
+
+            <TextInput style = {styles.input} autoCapitalize = "none" value = {""} onChange={""} placeholder='Email@example.com' keyboardType='email-address'> 
+    
+            </TextInput>
+
+            <TextInput style = {[styles.inputMessage, styles.MultilineText]} autoCapitalize = "none" value = {""} onChange={""} placeholder="Message here" multiline keyboardType='email-address'> 
+    
+            </TextInput>
+            
+            <TouchableOpacity>
+
+              <Text style = {{ fontWeight: 'bold', fontSize: 20, color: '#003153'}}>
+                Send
+              </Text>
+
+            </TouchableOpacity>
+
+           
+     
+
+
+
+           </View>
+
+           
         
 
-      </View>
 
-      <View style = {styles.features}>
 
-        <Text style = {{ paddingTop: 1, textAlign: 'justify', fontWeight: 'bold', fontSize: 20,}}>MBRAILLE <Text style = {{color: '#062CD4'}}> FEATURES</Text></Text>
+
+
       
-      </View>
+      
+      
+    </ScrollView>
 
-      {/*FEAAAAAATURESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS*************/}
-
-      <ScrollView 
-                    horizontal
-                    showsHorizontalScrollIndicator={false}
-                    style={{height:250}}
-                >         
-                    <View
-                        style={{
-                            height:100,
-                            elevation:2,
-                            backgroundColor: "#EBF0F5",
-                            marginLeft:20,
-                            marginTop:20,
-                            borderRadius: 8 ,
-                            marginBottom: 10,
-                            width: 150,
-                            alignContent: 'center',
-                            alignItems: 'center',
-                            alignSelf: 'center',
-                            paddingTop: 10,
-                            
-                        }}
-                    >
-                        <Image
-                            source={require('../assets/maineIcons/multiple.png')}
-                            style = {{ width: 50, height: 50, alignSelf: 'center', }}
-                        />
-                        <View style={{
-                            flexDirection:"row",
-                            paddingTop:15,
-                            paddingHorizontal:10,
-                            alignSelf: 'center',
-
-                        }}>
-                            <Text style={{
-                                fontWeight:"bold",
-                                fontSize: 11,
-                        
-                            
-                                
-                            }}>Multimodel Transcription</Text>          
-                        </View>
-                    </View>
-
-
-                    <View
-                        style={{
-                            height:100,
-                            elevation:2,
-                            backgroundColor: "#EBF0F5",
-                            marginLeft:20,
-                            marginTop:20,
-                            borderRadius: 8 ,
-                            marginBottom:10,
-                            width: 150,
-                            alignContent: 'center',
-                            alignItems: 'center',
-                            alignSelf: 'center',
-                            paddingTop: 10,
-                        }}
-                    >
-                        <Image
-                            source={require('../assets/maineIcons/easytouse.png')}
-                            style = {{ width: 50, height: 50, alignSelf: 'center', }}
-                        />
-                        <View style={{
-                            flexDirection:"row",
-                            paddingTop:10,
-                            paddingHorizontal:10,
-                            alignSelf: 'center'
-                        }}>
-                            <Text style={{
-                                fontWeight:"bold",
-                                fontSize: 12,
-                                paddingTop: 4
-                                
-                            }}>Easy to Use</Text>          
-                        </View>
-                    </View>
-
-
-                    <View
-                        style={{
-                            height:100,
-                            elevation:2,
-                            backgroundColor: "#EBF0F5",
-                            marginLeft:20,
-                            marginTop:20,
-                            borderRadius: 8 ,
-                            marginBottom:10,
-                            width: 150,
-                            alignContent: 'center',
-                            alignItems: 'center',
-                            alignSelf: 'center',
-                            paddingTop: 10,
-                        }}
-                    >
-                        <Image
-                            source={require('../assets/maineIcons/accurate.png')}
-                            style = {{ width: 50, height: 50, alignSelf: 'center',  }}
-                        />
-                        <View style={{
-                            flexDirection:"row",
-                            paddingTop:10,
-                            paddingHorizontal:10,
-                            alignSelf: 'center'
-                        }}>
-                            <Text style={{
-                                fontWeight:"bold",
-                                fontSize: 12,
-                                paddingTop: 4
-                                
-                            }}>Accurate</Text>          
-                        </View>
-                    </View>
-
-      </ScrollView>  
-
-
-      <View style = {{paddingTop: 5,}}>
-
-      <Text style = {{textAlign: 'justify', fontWeight: 'bold', fontSize: 20, alignSelf: 'center' }}>THE <Text style = {{color: '#062CD4'}}>DEVELOPERS</Text></Text>
-
-
-      </View>
 
 
     
-
-
-
-       <View style = {styles.developers}>
-
-
-  
-              <View style = {styles.box}>
-                <View style = {styles.inner}>
-                <Image style={{ height: 55, width: 55 , borderRadius: 50  }} source={require('../assets/DEVELOPERS/GOMEZ.jpg')} />
-                  <Text style = {styles.text}>Rafael Gomez</Text>
-                  <Text style = {{ fontStyle: 'italic', color: 'white', fontSize: 12}}>Data Science</Text>
-                </View>
-              </View>
-
-
-              <View style = {styles.box}>
-                <View style = {styles.inner}>
-                <Image style={{ height: 55, width: 55, borderRadius: 50  }} source={require('../assets/DEVELOPERS/MACADANGDANG.jpg')} />
-                  <Text style = {styles.text}>Aldrich Macadangdang</Text>
-                  <Text style = {{ fontStyle: 'italic', color: 'white', fontSize: 12}}>Railway Engineering</Text>
-                </View>
-              </View>
-
-
-              <View style = {styles.box}>
-                <View style = {styles.inner}>
-                <Image style={{ height: 55, width: 55 , borderRadius: 50  }} source={require('../assets/DEVELOPERS/AGUINALDO.jpg')} />
-                <Text style = {styles.text}>David Aguinaldo</Text>
-                <Text style = {{ fontStyle: 'italic', color: 'white', fontSize: 12}}>Railway Engineering</Text>
-                  
-                </View>
-                </View>
-
-
-              <View style = {styles.box}>
-                <View style = {styles.inner}>
-                <Image style={{ height: 55, width: 55 , borderRadius: 50  }} source={require('../assets/DEVELOPERS/PASCUA.jpg')} />
-                  <Text style = {styles.text}>Mark Pascua</Text>
-                  <Text style = {{ fontStyle: 'italic', color: 'white', fontSize: 12}}>System Adminitration</Text>
-
-                </View>
-              </View>
-
-
-            
-        </View>
-
-
-
-
-
-
- 
-
-
-
-      </ScrollView>
-  
   )
 }
 
-const styles = StyleSheet.create({ 
+const styles = ScaledSheet.create({
 
-content: {
-  alignItems: 'center',
-  flex: 1,
+
+// MAIN CONTAINER
+
+
+main: {
+  padding: '8@s' ,
+  backgroundColor: 'white'
+
+},
+
+
+
+
+// DEVELOPERS ****************************************
+
+headerDeveloper: {
+  paddingTop: '5@s',
   alignContent: 'center',
+  alignItems:'center',
 
-
-},
-
-background: {
-
-  height: 250,
-  width: 411,
-  borderBottomLeftRadius: 22,
-  borderBottomRightRadius: 22,
-
-},
-
-whoarewe: {
-  padding: 19,
-
-
-},
-
-features: {
   
-  alignSelf: 'center'
+},
+
+headerTitleDeveloper: { // HEADER TITLEEEEEEEEEEEE
+  fontSize: '24@s',
+  fontWeight: 'bold',
+  color: '#003153',
+  paddingBottom: '15@s',
+  paddingTop: '15@s'
 
 
 },
 
-// developers
-
-developers: {
-  width: '100%',
-  height: '30%',
-  padding: 20,
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  alignSelf: 'center',
-  paddingTop: 35
-},
-
-box: {
-
-  width: '50%',
-  height: '50%',
-  padding: 5,
-  borderRadius: 8, 
+name: {
+  fontSize: "10@s",
+  fontWeight: 'bold', 
+  color: 'white',
 
 
 },
 
-inner: {
-  flex: 1,
-  backgroundColor: '#062CD4',
-  alignItems: 'center',
-  justifyContent: 'center',
+
+electives: {
+  fontSize: "9@s",
+  fontStyle: 'italic',
+  color: 'white',
+  top: '-5@s'
+
+},
+
+cardfavorites: {
+  height: '40@s',
+  width: '40@s',
   borderRadius: 8,
 
 
 },
 
-text: {
+card: {
+  flex: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '80@s',
+  height: '95@s',
+  borderRadius: 8, 
+  margin: "2@s",
+  gap: '3@s',
+  
 
-  color: 'white',
+},
+
+cardElevated: {
+  backgroundColor:  '#003153', // bg color of card hehe
+  elevation: 5,
+  shadowOffset: {
+  width: '10@s',
+  height: '10@s',
+
+  },
+
+  shadowColor: 'black',
+  shadowOpacity: 10 ,
+  shadowRadius: 50,
+
+},
+
+// what is mobile braille
+
+mobileBraille: {
+  textAlign: 'justify',
+  fontSize: '15@s',
+  marginTop: '10@s',
+  color: '#003153'
+},
+
+// QUICK CONTACT
+
+quickContact: {
+  paddingTop: '5@s',
+  alignContent: 'center',
+  alignItems:'center',
+
+},
+
+quickContactHeader: {
+
+  fontSize: '24@s',
   fontWeight: 'bold',
-  fontSize: 12,
+  color: '#003153',
+  paddingBottom: '12@s',
+  paddingTop: '10@s'
+  
+},
+
+// form and input ******************
+
+form: {
+  padding: '8@s' ,
+
+},
+
+input: {
+  backgroundColor: "#EBF0F5" ,
+
+  height: 35,
+
+},
+
+inputMessage: {
+
+  borderColor: '#003153',
+  backgroundColor: "#EBF0F5" ,
 
 
-}
+},
+
+MultilineText: {
+  minHeight: 100,
+  textAlignVertical: "top",
+
+},
 
 
 
 
 
 
+
+
+
+
+
+  
 })
+    
 
-export default About
+export default about
