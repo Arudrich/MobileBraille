2294655ea901e33baaf064353bc208b35a4b40ee
Aru <aldrichdmacadangdang11@gmail.com>
2024-02-17 17:10:07 +0800

    UI SPLASH

---
+++ src/screen/historyScreen.js
@@ -0,0 +1,26 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+
+// navigation to another screen
+import { useNavigation } from '@react-navigation/native';
+import { AuthContext } from '../../navigation/AuthProvider';
+
+
+
+
+
+
+
+
+const historyScreen = () => { 
+    
+
+  return (
+    <View>
+      <Text>historyScreen</Text>
+    </View>
+  )
+}
+
+export default historyScreen
