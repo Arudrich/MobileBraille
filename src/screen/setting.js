673dd909a55d12dc9964d78050503772f48a37e2
Aru <aldrichdmacadangdang11@gmail.com>
2024-02-18 18:44:14 +0800

    settings UI Update

--- src/screen/setting.js
+++ src/screen/setting.js
@@ -231,17 +231,19 @@ logoContainer: {
   alignItems: 'center',
   borderBottomLeftRadius: 22,
   borderBottomRightRadius: 22,
+  backgroundColor: '#003153'
   
 },
 logo: {
   width: 110, // Adjust the width as needed
   height: 180, // Adjust the height as needed
-  tintColor: '#003153',
+  tintColor: 'white',
 },
 
 // section
   section: {
     paddingHorizontal: '8@s',
+    marginTop: '6@s',
   },
   sectionHeader: {
     // paddingVertical: 5,
