2a897033e70f4b4405fab471c5b2453e8901d18e
DavidA. <dvd.agnld@gmail.com>
2024-02-19 13:01:49 +0800

    asdasd

--- src/screen/SubmittedPostScreen.js
+++ src/screen/SubmittedPostScreen.js
@@ -10,25 +10,33 @@ import { Button } from 'react-native-paper';
 const SubmittedPostScreen = ({ route }) => {
   const { title, imageUrl, transcription, braille, transcriptionType} = route.params;
 
-  return (
-    <ScrollView style={{ flex: 1, }}>
-      <View style={{ alignItems: 'center', justifyContent: 'center'}}>
+   return (
+    <ScrollView style={{ flex: 1 }}>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
         <Text>Title: {title}</Text>
+
         {transcriptionType === 'video' && <Video source={{ uri: imageUrl }} style={{ width: 300, height: 200 }} useNativeControls />}
         {transcriptionType === 'image' && <Image source={{ uri: imageUrl }} style={{ width: 200, height: 200, marginVertical: 20 }} resizeMode='contain' />}
         {transcriptionType === 'audio' && <Audio source={{ uri: imageUrl }} />}
-        <Text>Transcription: {transcription}</Text>
-        <Text>Braille: {braille}</Text>
-      </View>
 
-        {/* BUTTONS FOR DOWNLOAD NG BRF AND DOC */}
+        <ScrollView contentContainerStyle={styles.resultBox}>
+          <Text>Transcription:{'\n'}{'\n'} {transcription}</Text>
+          <Text>Braille:{'\n'}{'\n'} {braille}</Text>
+        </ScrollView>
+
         <View style={styles.buttonContainer}>
-        <Button icon="download" mode="elevated" onPress={() => console.log('Pressed')} style={{width: 125}} textColor= '#003153'>Transcript</Button>
-        <Button icon="download" mode="elevated" onPress={() => console.log('Pressed')} style={{ width: 100 }} textColor= '#003153'>BRF</Button>
-        <Button icon="download" mode="elevated" onPress={() => console.log('Pressed')} style={{ width: 100 }} textColor= '#003153'>PEF</Button>
+          <Button icon="download" mode="elevated" onPress={() => console.log('Pressed')} style={{ width: 125 }} textColor="#003153">
+            Transcript
+          </Button>
+          <Button icon="download" mode="elevated" onPress={() => console.log('Pressed')} style={{ width: 100 }} textColor="#003153">
+            BRF
+          </Button>
+          <Button icon="download" mode="elevated" onPress={() => console.log('Pressed')} style={{ width: 100 }} textColor="#003153">
+            PEF
+          </Button>
         </View>
+      </View>
     </ScrollView>
-    
   );
 };
 
@@ -38,8 +46,16 @@ const styles = ScaledSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginVertical: '150@s', 
-    paddingHorizontal: '35@s', 
+    marginVertical: '7@s', 
+    paddingHorizontal: '30@s', 
+  },
+  resultBox: {
+    borderWidth: 3,
+    borderColor: '#003153',
+    padding: 10,
+    borderRadius: 8,
+    width: '90%', // Adjust the width as needed
+    alignItems: 'left',
   },
 });
 
