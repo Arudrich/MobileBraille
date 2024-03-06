3bec60fd11b6a565d16f6997fb315211f97522cc
Aru <aldrichdmacadangdang11@gmail.com>
2024-03-06 15:54:54 +0800

    View post and Add post UI adjustments

--- src/screen/SubmittedPostScreen.js
+++ src/screen/SubmittedPostScreen.js
@@ -110,31 +110,61 @@ import { Audio, Video } from 'expo-av';
 import { ScaledSheet } from 'react-native-size-matters';
 import { Button } from 'react-native-paper';
 
+// Custom Fonts ********************
+
+import { useFonts } from 'expo-font'
+
 const SubmittedPostScreen = ({ route }) => {
   const { title, imageUrl, transcription, braille, transcriptionType, downloadLinks } = route.params;
+
+  // fonts*******************************************************
+
+const [ fontsLoaded ] = useFonts({
+    'PTSans-Bold' : require ('../assets/fonts/PTSans-Bold.ttf'),
+    'PTSans-BoldItalic' : require ('../assets/fonts/PTSans-BoldItalic.ttf'),
+    'PTSans-Italic' : require ('../assets/fonts/PTSans-Italic.ttf'),
+    'PTSans-Regular' : require ('../assets/fonts/PTSans-Regular.ttf'),
+
+
+  })
+
+  if (!fontsLoaded){
+    return undefined ;
+  }
+
 
   return (
     <ScrollView style={{ flex: 1 }}>
       <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+
+        <Text style = {styles.header}>Transcription Results</Text>
+
+
         <Text style={styles.textStyle}>Title: {title}</Text>
+        <Text style={styles.textStyle}>Date: March 10, 2024 </Text>
+        <Text style={styles.textStyle}>Type of Transcription: Mark Pascua to David Aguinaldo  </Text> 
 
         {transcriptionType === 'video' && <Video source={{ uri: imageUrl }} style={styles.videoContainer} useNativeControls />}
         {transcriptionType === 'image' && <Image source={{ uri: imageUrl }} style={styles.imageContainer} resizeMode='contain' />}
 
         <ScrollView contentContainerStyle={styles.resultBox}>
-          <Text style={styles.textStyleOne}>Transcription:{'\n'}{'\n'} {transcription}</Text>
-          <Text style={styles.textStyleOne}>Braille:{'\n'}{'\n'} {braille}</Text>
+          <Text style={styles.textStyleOne}>Transcription Input:{'\n'}{'\n'} {transcription}</Text>
+        </ScrollView>
+
+        <ScrollView contentContainerStyle={styles.resultBox}>
+         
+          <Text style={styles.textStyleOne}>Braille Output:{'\n'}{'\n'} {braille}</Text>
         </ScrollView>
 
         <View style={styles.buttonContainer}>
           <Button icon="download" mode="elevated" onPress={() => Linking.openURL(downloadLinks.doc)} style={[styles.button, { width: 125 }]} textColor="#003153">
-            Transcript
+          <Text style = {styles.fontDownload}>Transcript</Text>
           </Button>
           <Button icon="download" mode="elevated" onPress={() => Linking.openURL(downloadLinks.brf)} style={[styles.button, { width: 100 }]} textColor="#003153">
-            BRF
+          <Text style = {styles.fontDownload}>BRF</Text>
           </Button>
           <Button icon="download" mode="elevated" onPress={() => Linking.openURL(downloadLinks.pef)} style={[styles.button, { width: 100 }]} textColor="#003153">
-            PEF
+          <Text style = {styles.fontDownload}>PEF</Text>
           </Button>
         </View>
       </View>
@@ -143,41 +173,75 @@ const SubmittedPostScreen = ({ route }) => {
 };
 
 // Button Styles
+
 const styles = ScaledSheet.create({
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     marginVertical: '15@s',
-    paddingHorizontal: 10,
   },
   button: {
-    marginRight: '10@s', // Adjust the margin as needed
+    marginRight: '8@s', // Adjust the margin as needed
   },
   resultBox: {
-    borderWidth: 3,
-    borderColor: '#003153',
-    padding: 10,
-    borderRadius: 8,
-    width: '90%', // Adjust the width as needed
-    alignItems: 'flex-start',
+      borderWidth: 3.5,
+      borderColor: '#003153',
+      padding: '12@s',
+      borderRadius: 8,
+      width: '80%', // Adjust the width as needed
+      alignItems: 'flex-start',
+      top: '12@s',
+      margin: '15@s',
   },
-  textStyle: {
-    fontSize: '14@s',
-    fontWeight: 'bold',
+
+   // style to sa text liek title and dates pati pala type of transciropotion hehe
+
+   textStyle: {
+      fontSize: '14@s',
+      fontFamily: "PTSans-Regular",
+      alignSelf: 'flex-start',
+      padding: 8
   },
-  textStyleOne: {
-    fontSize: '12@s',
-  },
+      textStyleOne: {
+      fontSize: '11@s',
+      fontFamily: "PTSans-Regular",
+      textAlign: 'justify'
+
+      },
   videoContainer: {
-    width: '300@s',
-    height: '200@s',
+      width: '300@s',
+      height: '200@s',
+      alignSelf: 'center',
+      borderWidth: 3,
+      borderColor: '#003153',
+      borderRadius: 8,   
+    
   },
+
   imageContainer: {
-    width: '200@s',
-    height: '200@s',
-    marginVertical: '20@s'
+      width: '300@s',
+      height: '250@s',
+      marginVertical: '20@s',
+      borderWidth: 3,
+      borderColor: '#003153',
+      borderRadius: 8, 
   },
+
+  header: {
+      fontSize: '25@s',
+      fontFamily: "PTSans-Bold",
+      alignSelf: 'flex-start', 
+      padding: 8
+  },
+
+  fontDownload: {
+      fontSize: '12@s',
+      fontFamily: "PTSans-Bold",
+
+  }
+
+
 });
 
 export default SubmittedPostScreen;
--- src/screen/ViewPostScreen.js
+++ src/screen/ViewPostScreen.js
@@ -5,31 +5,63 @@ import { Video } from 'expo-av';
 
 import { ScaledSheet } from 'react-native-size-matters';
 
+// Custom Fonts ********************
+
+import { useFonts } from 'expo-font'
+
 const ViewPostScreen = ({ route }) => {
   const { title, imageUrl, transcription, braille, transcriptionType, downloadLinks } = route.params;
 
+  // fonts*******************************************************
+
+  const [ fontsLoaded ] = useFonts({
+    'PTSans-Bold' : require ('../assets/fonts/PTSans-Bold.ttf'),
+    'PTSans-BoldItalic' : require ('../assets/fonts/PTSans-BoldItalic.ttf'),
+    'PTSans-Italic' : require ('../assets/fonts/PTSans-Italic.ttf'),
+    'PTSans-Regular' : require ('../assets/fonts/PTSans-Regular.ttf'),
+
+
+  })
+
+  if (!fontsLoaded){
+    return undefined ;
+  }
+
+
   return (
+
     <ScrollView style={{ flex: 1 }}>
       <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={styles.textStyle}>Title: {title}</Text>
 
-        {transcriptionType === 'video' && <Video source={{ uri: imageUrl }} style={styles.videoContainer} useNativeControls />}
-        {transcriptionType === 'image' && <Image source={{ uri: imageUrl }} style={styles.imageContainer} resizeMode='contain' />}
+        <Text style = {styles.header}>Latest Transcription</Text>
+
+
+        <Text style={styles.textStyle}>Title: {title} </Text>
+        <Text style={styles.textStyle}>Date: March 10, 2024 </Text>
+        <Text style={styles.textStyle}>Type of Transcription: Mark Pascua to David Aguinaldo  </Text> 
+
+        {transcriptionType === 'video' && <Video source={{ uri: imageUrl }} style={styles.videoContainer} resizeMode= 'contain' useNativeControls />}
+        {transcriptionType === 'image' && <Image source={{ uri: imageUrl }} style={styles.imageContainer} resizeMode= 'contain' />}
 
         <ScrollView contentContainerStyle={styles.resultBox}>
-          <Text style={styles.textStyleOne}>Transcription:{'\n'}{'\n'} {transcription}</Text>
-          <Text style={styles.textStyleOne}>Braille:{'\n'}{'\n'} {braille}</Text>
+          <Text style={styles.textStyleOne}>Transcription Input:{'\n'}{'\n'} {transcription}</Text>
+        </ScrollView>
+
+        <ScrollView contentContainerStyle={styles.resultBox}>
+         
+          <Text style={styles.textStyleOne}>Braille Output:{'\n'}{'\n'} {braille}</Text>
+
         </ScrollView>
 
         <View style={styles.buttonContainer}>
           <Button icon="download" mode="elevated" onPress={() => Linking.openURL(downloadLinks.doc)} style={[styles.button, { width: 125 }]} textColor="#003153">
-            Transcript
+            <Text style = {styles.fontDownload}>Transcript</Text>
           </Button>
           <Button icon="download" mode="elevated" onPress={() => Linking.openURL(downloadLinks.brf)} style={[styles.button, { width: 100 }]} textColor="#003153">
-            BRF
+            <Text style = {styles.fontDownload}>BRF</Text>
           </Button>
           <Button icon="download" mode="elevated" onPress={() => Linking.openURL(downloadLinks.pef)} style={[styles.button, { width: 100 }]} textColor="#003153">
-            PEF
+            <Text style = {styles.fontDownload}>PEF</Text>
           </Button>
         </View>
       </View>
@@ -43,35 +75,74 @@ const styles = ScaledSheet.create({
       justifyContent: 'space-between',
       alignItems: 'center',
       marginVertical: '15@s',
-      paddingHorizontal: 10,
     },
     button: {
-      marginRight: '10@s', // Adjust the margin as needed
+      marginRight: '8@s', // Adjust the margin as needed
     },
     resultBox: {
-      borderWidth: 3,
+      borderWidth: 3.5,
       borderColor: '#003153',
-      padding: 10,
+      padding: '12@s',
       borderRadius: 8,
-      width: '90%', // Adjust the width as needed
+      width: '80%', // Adjust the width as needed
       alignItems: 'flex-start',
+      top: '12@s',
+      margin: '15@s',
     },
+
+    // style to sa text liek title and dates pati pala type of transciropotion hehe
     textStyle: {
       fontSize: '14@s',
-      fontWeight: 'bold',
+      fontFamily: "PTSans-Regular",
+      alignSelf: 'flex-start',
+      padding: 8
     },
+    
+
+
     textStyleOne: {
-      fontSize: '12@s',
+      fontSize: '11@s',
+      fontFamily: "PTSans-Regular",
+      textAlign: 'justify'
+
     },
     videoContainer: {
       width: '300@s',
       height: '200@s',
+      alignSelf: 'center',
+      borderWidth: 3,
+      borderColor: '#003153',
+      borderRadius: 8, 
+     
     },
     imageContainer: {
-      width: '200@s',
-      height: '200@s',
-      marginVertical: '20@s'
+      width: '300@s',
+      height: '250@s',
+      marginVertical: '20@s',
+      borderWidth: 3,
+      borderColor: '#003153',
+      borderRadius: 8, 
     },
+
+    //header ************
+    
+    header: {
+      fontSize: '25@s',
+      fontFamily: "PTSans-Bold",
+      alignSelf: 'flex-start', 
+      padding: 8
+    },
+
+    fontDownload: {
+      fontSize: '12@s',
+      fontFamily: "PTSans-Bold",
+
+    }
+
+
+
+
+
   });
 
   export default ViewPostScreen;
