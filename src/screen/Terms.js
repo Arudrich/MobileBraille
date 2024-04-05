c64382b75ad5b70c89c25bd53925501bb2b49cf3
DavidA. <dvd.agnld@gmail.com>
2024-04-05 09:41:45 +0800

    commitment

--- src/screen/Terms.js
+++ src/screen/Terms.js
@@ -8,13 +8,10 @@ import { ScrollView } from 'react-native'
 const Terms = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text style = {styles.Title}>Terms and Condition</Text>
-      </View>
 
       <ScrollView  showsVerticalScrollIndicator={false} >
       
-            <Text style = {styles.Header}>Terms and Conditions</Text>
+              <Text style = {styles.Header}>Terms and Condition</Text>
 
               <Text style = {styles.text}>
 
@@ -23,10 +20,10 @@ const Terms = () => {
                 Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users, and others who access or use the Service.
 
                 By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service.
-              
+                {'\n'}
               </Text>
            
-            <Text style = {styles.Header}>TContent</Text>
+            <Text style = {styles.Header}>Content</Text>
 
               <Text style = {styles.text}>
 
@@ -41,10 +38,10 @@ const Terms = () => {
               <Text style = {styles.text}>
 
               We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material, we will try to provide at least 30 days' notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.
-
+              {'\n'}
               </Text>
 
-            <Text style = {styles.Header}>TContact Us</Text>
+            <Text style = {styles.Header}>Contact Us</Text>
 
               <Text style = {styles.text}>
 
@@ -63,20 +60,30 @@ const Terms = () => {
 export default Terms
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  text: {
+    textAlign: 'justify',
+    fontSize: 14,
+    marginLeft: 15,
+    marginRight: 20
+  },
 
   Title: {
 
   },
 
   Header: {
-
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    marginLeft: 5,
 
   },
-
-  text: {
-
-    
-  }
 
 
 
